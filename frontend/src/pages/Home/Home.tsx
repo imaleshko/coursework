2954cae57d44f0d058bee8df175ae5a0f3c8be18
ivan_maleshko <183198079+imaleshko@users.noreply.github.com>
2026-03-27 00:44:20 +0200

    feat: add Home page

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,5 +1,29 @@
 import styles from "./Home.module.css";
+import { useState } from "react";
+import { Card } from "@/components/Home/Card/Card";
 
 export const Home = () => {
-  return <div></div>;
+  const [search, setSearch] = useState("");
+
+  return (
+    <div className={styles.wrapper}>
+      <p className={styles.text1}>Шукай</p>
+      <div className={styles.searchGroup}>
+        <input
+          className={styles.input}
+          name="search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button className={styles.button}>Пошук</button>
+      </div>
+      <p className={styles.text2}>або переглянь нові</p>
+
+      <div className={styles.cards}>
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
 };
