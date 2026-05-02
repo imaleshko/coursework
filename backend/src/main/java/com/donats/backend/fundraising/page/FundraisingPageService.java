a26cf0e9724513e6ad564b9923690d459735888e
ivan_maleshko <maleshko.ivan@student.uzhnu.edu.ua>
2026-05-02 18:14:21 +0300

    feat: add backend for fundraising page, fundraising page skeleton and carousel component

---
+++ backend/src/main/java/com/donats/backend/fundraising/page/FundraisingPageService.java
@@ -0,0 +1,38 @@
+package com.donats.backend.fundraising.page;
+
+import com.donats.backend.entities.FundraisingEntity;
+import com.donats.backend.fundraising.page.dto.FundraisingResponseDto;
+import com.donats.backend.fundraising.page.exception.FundraisingNotFoundException;
+import com.donats.backend.repositories.FundraisingRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class FundraisingPageService {
+
+    private final FundraisingRepository fundraisingRepository;
+
+    public FundraisingPageService(FundraisingRepository fundraisingRepository) {
+        this.fundraisingRepository = fundraisingRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public FundraisingResponseDto getFundraisingByUsernameAndSlug(String username, String slug) {
+        FundraisingEntity fundraising = fundraisingRepository.findByUserUsernameAndSlug(username, slug)
+                .orElseThrow(() -> new FundraisingNotFoundException("Fundraising not found"));
+
+        return new FundraisingResponseDto(
+                fundraising.getId(),
+                fundraising.getTitle(),
+                fundraising.getSlug(),
+                fundraising.getDescription(),
+                fundraising.getBalance(),
+                fundraising.getGoal(),
+                fundraising.getEndDate(),
+                fundraising.getImagesUrl(),
+                fundraising.getUser().getUsername(),
+                fundraising.getStatus().name(),
+                fundraising.getStartedAt()
+        );
+    }
+}
--- frontend/src/pages/Fundraising/Fundraising.tsx
+++ frontend/src/pages/Fundraising/Fundraising.tsx
@@ -1,1 +1,34 @@
-export const Fundraising = () => <div>Fundraising</div>;
+import styles from "./Fundraising.module.css";
+import { Link, useParams } from "react-router";
+import { useQuery } from "@tanstack/react-query";
+import { fundraisingApi } from "@/api/fundraisingApi.ts";
+import Carousel from "@/pages/Fundraising/Components/Carousel/Carousel.tsx";
+
+export const Fundraising = () => {
+  const { username, slug } = useParams();
+
+  const { data: fundraising } = useQuery({
+    queryKey: ["fundraising", username, slug],
+    queryFn: () => fundraisingApi.getByUsernameAndSlug(username!, slug!),
+  });
+
+  if (!fundraising) return null;
+
+  return (
+    <div className={styles.wrapper}>
+      <Link to={`/fundraising/${username}/${slug}`} className={styles.title}>
+        {fundraising.title}
+      </Link>
+
+      <div className={styles.divider}></div>
+
+      <div className={styles.topSection}>
+        <div className={styles.carouselContainer}>
+          <Carousel imagesUrls={fundraising.imagesUrl} />
+        </div>
+
+        <div className={styles.infoContainer}></div>
+      </div>
+    </div>
+  );
+};
